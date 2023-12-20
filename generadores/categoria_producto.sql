294b158dca8165b4111be141e5ce239a3e93c12d
Mundus <julianvent32@gmail.com>
2023-12-20 13:11:56 -0600

    categorias

---
+++ generadores/categoria_producto.sql
@@ -0,0 +1,15 @@
+INSERT INTO categoria_producto (
+    nombre
+)
+VALUES
+    ('Pase de temporada')
+    , ('Boleto de un Día')
+    , ('Plan de alimentos')
+    , ('Flash Pass')
+    , ('Estacionamiento')
+    , ('VIP')
+    , ('Grupos')
+    , ('Six Flags Plus')
+    , ('Paquete de media digital')
+    , ('Transporte')
+;
