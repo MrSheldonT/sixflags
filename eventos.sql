9a8018e6fdafebd5c83460a8dd5561cfabaf0276
Sinhularity <zS22017021@estudiantes.uv.mx>
2023-12-20 22:16:45 -0600

    Update datos de servicios y eventos...

---
+++ eventos.sql
@@ -0,0 +1,76 @@
+
+INSERT INTO evento (parque_id, nombre, descripcion, patrocinador, fecha_inicio, fecha_fin)
+VALUES
+        -- S.F México ¿ Exclusive?--
+        (1,'Christmas In the Park','La temporada más increíble del año regresa para iluminar y llenar de magia Six Flags México','Six Flags','2023-11-24','2024-01-21')
+        , (1,'Gaming & Coaster Fest','Llega por primera vez un universo que reúne la adrenalina de tus juegos mecánicos favoritos con la diversión del mundo Gaming.','Coca Cola','2024-02-16','2024-04-07')
+        , (1,'Fun Nights','Disfruta de horas extra de diversión después del cierre de nuestro parque. Música, Mariachi y algunos juegos mecánicos te esperan para pasar las mejores noches en familia.','Six Flags','2024-03-08','2024-05-26')
+        , (1,'Flavors of the World','No te pierdas por primera vez este delicioso festival donde los sabores del mundo se mezclan para deleitar tu paladar.','Six Flags','2024-03-08','2024-05-26')
+        , (1,'Héroes y Villanos Fest','¡Una nube de colores regresa esta primavera y viene acompañada de Héroes, Villanos y por supuesto tus personajes favoritos de los Looney Tunes!','Coca Cola','2024-04-20','2024-06-30')
+        , (1,'Six Flags Led Lights & Laser Show','Una sinfonía de colores y rayos láser invade Six Flags México, vive esta mágica presentación dirigida por tus personajes favoritos.','Six Flags','2024-07-06','24-08-25')
+        , (1,'Festival del Terror','Siniestras Criaturas se reúnen en el evento más escalofriante del año. ¡No podrás escapar de ellos!','Fanta','2024-09-12','2024-10-10')
+        , (1,'Christmas in the Park 2024','¡La temporada más mágica regresa a iluminar la Navidad y llenar de alegría a todas las familias!','Six Flgas','2024-11-22','2025-01-19')
+        , (1,'Kids Boo Fest','¡Kid\'s Boo Fest es un evento de Halloween deliciosamente espeluznante y familiar para todas las edades! Los más pequeños pueden embarcarse en nuestro sendero de truco o trato, decoraciones festivas y actividades interactivas como pintura de calabazas y concursos de disfraces. Es una manera perfecta de abrazar el espíritu de Halloween con entretenimiento que garantiza sonrisas, risas y momentos memorables para toda la familia.','Six Flags','2024-09-12','2024-10-10')
+    -- H.H Oaxtepec (La mayoría de H.H no tiene eventos)
+        , (1,'Fun Nights','Disfruta de horas extras de diversión al cierre del parque, vive el mejor ambiente familiar con música en vivo, comida y un increíble show de rayos láser.','Six Flags','2024-03-09','2024-05-26')
+        , (1,'Pinic Fest','Arma tu Picnic con tu familia, amigos y amigas y disfruta de un día increíble en el Oasis Tropical.','Six Flags','2024-07-04','2024-08-25')
+        , (1,'Kids Boo Fest','¡Kid\'s Boo Fest es un evento de Halloween deliciosamente espeluznante y familiar para todas las edades! Los más pequeños pueden embarcarse en nuestro sendero de truco o trato, decoraciones festivas y actividades interactivas como pintura de calabazas y concursos de disfraces. Es una manera perfecta de abrazar el espíritu de Halloween con entretenimiento que garantiza sonrisas, risas y momentos memorables para toda la familia.','Six Flags','2024-09-12','2024-10-10')
+        , (1,'Festival del Terror','Siniestras Criaturas se reúnen en el evento más escalofriante del año. ¡No podrás escapar de ellos!','Fanta','2024-09-12','2024-10-10')
+        , (1,'Christmas in the Park 2024','¡La temporada más mágica regresa a iluminar la Navidad y llenar de alegría a todas las familias!','Six Flgas','2024-11-22','2025-01-19')
+    -- S.F Discovery Kingdom ¿ Exclusive? --
+        , (2,'One Warm Coat','Donate a new or gently worn coat to redeem a ticket at a discounted price!','Six Flags','2024-11-24','2024-01-07')
+        , (2,'Scream Break','Experience night thrills at SCREAM Break! Watch the transformation of a thrill park into a spine-tingling, scare-filled experience for those seeking an adrenaline rush during spring break. Brace yourself for a chilling adventure as your Spring Break turns into SCREAM Break.','Six Flags','2024-03-16','2024-04-06')
+        , (2,'Viva La Fiesta','Bienvenido! Viva La Fiesta is a lively and colorful celebration that brings the festive spirit of Latin America to you! Immerse yourself in the rhythmic beats of salsa and mariachi music, indulge in delicious Latin cuisine, and enjoy captivating cultural performances!','Six Flags','2024-05-04','2024-06-02')
+        , (2,'Summer Nights Spectacular','Six Flags Discovery Kingdom\'s Summer Nights Spectacular is a dazzling summer event that combines the thrill of the season with a patriotic splendor. As the sun sets, get ready to light up the night with electrifying entertainment, and a festive atmosphere that celebrates the essence of summer and American pride. Join in the fun, enjoy exhilarating rides, and bask in the blend of summer delights and American cuisines!','Six Flags','2024-07-04','2024-07-13')
+        , (2,'Oktoberfest','Six Flags Discovery Kingdom\'s Oktoberfest is a lively and immersive celebration of German culture, bringing the world-famous beer festival to the heart of the theme park. Savor delicious German cuisine while enjoying live music, games and themed entertainment.','Six Flags','2024-09-14','2024-11-03')
+    -- S.F Over Georgia ¿ Exclusive?--
+        , (2,'Southern Star Music Festival','The Southern Star Music Festival was created in 1993 to provide a fun and educational festival for elementary, middle, and high school music groups. Attendees can purchase specially priced packages through Southern Star Music Festival so they can experience all the fun Six Flags Over Georgia has to offer!','Riverside EpiCenter Theatre','2024-03-16','2024-05-04')
+        , (2,'Music in the Parks','Spring 2024, come let your students shine during this wonderful performance opportunity with Music In The Parks. Play your hearts away at the musical festival and then enjoy a weekend’s worth of thrills, food and fun!','Six Flags','2024-04-05','2024-05-11')
+        , (2,'Six Flags Grad Nite','Six Flags Over Georgia Grad Nite offers more than any other Georgia Grad Nite destination. With once-in-a-lifetime experiences like world class coasters, high-energy dance areas, DJs and dinner buffet. The party doesn\'t stop there!','Six Flags','2024-05-03','2024-05-04')
+        , (2,'World of Illumination: Arctic Adventure','A magical holiday drive-through light experience in the parking lot of Six Flags Great America.','Six Flags','2024-11-17','2024-12-31')
+        , (2,'Physics Days','Take your classroom to new heights! Physics Day at Six Flags offer knowledge and fun in one sweet deal.','Six Flags','2024-04-25 09:00:00','2024-04-25 17:00:00')
+        , (2,'Math & Science Days','Take your classroom to new heights! Math & Science Days at Six Flags offer knowledge and fun in one sweet deal.','Six Flags','2024-04-27','2024-06-30')
+        , (2,'Dance Fest','Calling all performers! Dance Fest is back at Six Flags for 2024. Sign up and be a star.','Six Flags','2024-04-05','2024-06-02')
+        , (2,'Grad Nite','Dance, ride, and celebrate! Six Flags Grad Nite gives high school seniors a once-in-a-lifetime bash. Nobody gives you more thrills or better value!','Six Flags','2024-05-11','2024-05-18')
+    -- S.F America ¿ Exclusive? --
+        , (2,'Education Days','Take your classroom to new heights! Our Education Days at Six Flags offer knowledge and fun in one sweet deal.','Six Flags','2024-05-10','2024-05-31')
+    -- S.F New England ¿ Exclusive?
+        , (2,'July 4th Firework Celebration','Celebrate the biggest holiday of the summer at Six Flags with July 4th Fireworks presented by Coca-Cola.','Coca Cola','2024-07-04','2024-07-07')
+        , (2,'Food & Brew Festival','Join us as we celebrate our Food & Brew Festival! Our Food & Brew Festival promises to deliver OVER 50 diverse, flavorful brews, MORE specialty culinary creations, MORE entertainment, and MORE festival days to celebrate. Oh, and of course the best collections of coasters, thrill rides and water slides in New England! We\'ll see you at the festival! Cheers!','Six Flags','2024-07-06','2024-08-11')
+        , (2,'Oktoberfest','Explore German inspired cuisine, brews and deserts at New England\'s Oktoberfest Festival at Six Flags in Massachusetts','Six Flags','2024-09-07','2024-10-27')
+        , (2,'LAST SCARE WEEKEND','Get your last chance scares at some of your favorite haunts before the ghouls disappear before winter!','Snickers','2024-11-02','2024-11-03')
+    -- S.F St. Louis
+        , (2,'Low Sensory Sundays in April','Six Flags St. Louis has joined other parks around the country in adding a low sensory area to the park, along with other Autism-aware measures, during Autism Awareness Days.','Six Flags','2024-04-01','2024-04-30')
+      -- S.F Great Adventure
+        , (2,'Physics Days','Take your classroom to new heights! Physics Day at Six Flags offer knowledge and fun in one sweet deal.','Six Flags','2024-05-03 00:00:00','2024-05-03 20:59:59')
+        , (2,'Dance Fest','Bring your Dance Studio or Recreational Team to perform on stage at New Jersey\'s premier theme park!','Six Flags','2024-05-04 00:00:00','2024-05-04 20:59:59')
+        , (2,'Education Days','Take your classroom to new heights! Our Education Days at Six Flags offer knowledge and fun in one sweet deal.','Six Flags','2024-05-10','2024-05-17')
+        , (2,'50th Anniversary Celebration','Celebrate 50 years of great adventures and cherished memories since our opening in 1974. We\'re commemorating our semicentennial with not-to-be-missed attractions and events.','Six Flags','2024-06-01','2024-08-30')
+        , (2,'Grad Nite','Dance, ride, and celebrate! Six Flags Grad Nite gives high school seniors a once-in-a-lifetime bash. Nobody gives you more thrills or better value!','Six Flags','2024-06-07','2024-06-08')
+    -- S.F. Darian Lake
+        , (2,'Music Festivals','Six Flags Music Festivals are a great way to bring your performance season to a successful and rewarding conclusion.','Six Flags','2024-05-17','2024-06-08')
+    -- Frontier City
+        , (2,'Grad Nite','Dance, ride, and celebrate! Six Flags Grad Nite gives high school seniors a once-in-a-lifetime bash. Nobody gives you more thrills or better value!','Six Flags','2024-04-19','2024-04-20')
+        , (2,'Ride for Christ','Ride for the good Lord!','Six Flags','2024-04-21','2024-04-22')
+    -- S.F Over Texas
+        , (2,'The VIP Gazebo Experience','Spend your day in one of our VIP Gazebos.','Six Flags','2023-09-08','2023-12-31')
+        , (2,'Student Appreciation Days','Take your classroom to new heights! Education days offer special school rates, meal packages, and fun. Best field trip EVER!','Six Flags','2024-04-26','2024-05-24')
+        , (2,'Grad Nite','Dance, ride, and celebrate! Six Flags Grad Nite gives high school seniors a once-in-a-lifetime bash. Nobody gives you more thrills or better value!','Six Flags','2024-05-10','2024-05-11')
+    -- S.F Fiesta Texas
+       , (2,'New Year\'s 360 Fireworks Celebration Presented by ANCIRA™','This pyrotechnic spectacular presented by Ancira™ features a firework launch spanning across the quarry wall! Dec 31 Only','Ancira™','2023-12-31','2024-01-01')
+        , (2,'Diamond Bash: New Year''s Eve Pass Holder Event','As a valued Diamond pass holder and member, you are invited to this exclusive event filled with thrilling rides in the DC™ Universe and an amazing firework show at midnight.','Six Flags','2023-12-31','2024-01-01')
+        , (2,'Viva La Fiesta','Bienvenido! Viva La Fiesta is a lively and colorful celebration that brings the festive spirit of Latin America to you! Immerse yourself in the rhythmic beats of salsa and mariachi music, indulge in delicious Latin cuisine, and enjoy captivating cultural performances!','Six Flags','2024-04-29','2024-05-28')
+        , (2,'Summer Nights Spectacular','Leave your worries at home and join us for a summer celebration unlike any other.','Six Flags','2024-05-25','2024-08-11')
+        , (2,'Mardi Gras Festival™','The music, flavors, and culture of New Orleans are alive at Six Flags Fiesta Texas during Mardi Gras Festival. Enjoy live jazz music, street entertainers, and colorful décor along with all of your favorite rides.','Six Flags','2024-02-13','2024-02-25')
+        , (2,'Roller Coaster Rodeo','Roller Coaster Rodeo is an annual multi-day, mega fan event with exclusive ride time and behind the scenes tours! Tickets go on sale on January 1 at HIGH NOON!','Six Flags','2024-06-07','2024-06-09')
+        , (2,'4th of July Celebration','Let the excitement spark this 4th of July! Join us at Six Flags Fiesta Texas for a FOUR DAY celebration with rides, fireworks, music and more!','Six Flags','2024-07-04','2024-07-07')
+        , (2,'Oktoberfest Food Festival','Get your lederhosen and dirndls ready for the grandest Oktoberfest celebration yet! Indulge in traditional German foods and snacks while sipping on seasonal brews throughout the park. Fridays through Sundays from September 7 to October 27. With so much to eat, drink, and explore, the only thing missing is you.','Six Flags','2024-09-07','2024-10-27')
+    -- La Ronde único evento --
+        , (2,'L''International des Feux Loto-Québec','Montreal’s skies light up as the world’s greatest pyrotechnic artists take center stage in this international fireworks competition held every summer at La Ronde.','Tim Hortons & VIDÉCTRON','2024-06-29','2024-07-30')
+    -- ¿Utilizables en todos menos México? --
+    , (2,'Kids Boo Fest','Get ready for the best Halloween event of the season, featuring family-friendly thrills by day and the scariest frights by night!','Six Flags','2024-09-21','2024-11-03')
+    , (2,'Fright Fest','Experience the transformation and prepare for nights of frights into a realm of haunted horrors and eerie entertainment. With haunted mazes, terrifying scare zones, and hair-raising shows, it\'s a scream-inducing extravaganza for thrill-seekers. Enjoy the park''s rides in the dark, making it the ultimate destination chills of the Halloween season. *Some attractions and events require additional costs for admission','Snickers','2024-09-21','2024-11-03')
+                                    -- Christmas in the park para México--
+    , (2,'Holiday in the Park','Celebrate the magic of the season during Holiday in the Park where Six Flags Over Georgia will transform into a festive wonderland complete with millions of lights, scrumptious holiday treats, and classic live entertainment!','Six Flags','2024-11-22','2025-01-19')
+    , (2,'Spring Break','Kick off your Spring Break where you can enjoy thrilling rides, entertainment, and delicious treats! It\'s the perfect way to make lasting memories and kick off the spring season with a burst of excitement and adventure.','Six Flags','2024-03-16','2024-04-07')
+;
