9b7fdd80d7076c83c1465c8bbfd7d61643d2cda6
Mundus <julianvent32@gmail.com>
2023-12-21 03:45:23 -0600

    correciones

--- generadores/categoria_producto.sql
+++ generadores/categoria_producto.sql
@@ -1,18 +1,18 @@
 INSERT INTO categoria_producto (
-    nombre
+   nombre
 )
 VALUES
-    ('Pase de temporada')
-    , ('Boleto de un Día')
-    , ('Plan de alimentos')
-    , ('Estacionamiento')
-    , ('VIP')
-    , ('Grupos')
-    , ('Six Flags Plus')
-    , ('Paquete de media digital')
-    , ('Transporte')
-    , ('General')
-    , ('Membresía')
-    , ('Pase de temporada flexible')
-    , ('Cabaña')
+   ('Pase de temporada')
+   , ('Boleto de un Día')
+   , ('Plan de alimentos')
+   , ('Estacionamiento')
+   , ('VIP')
+   , ('Grupos')
+   , ('Six Flags Plus')
+   , ('Paquete de media digital')
+   , ('Transporte')
+   , ('General')
+   , ('Membresía')
+   , ('Pase de temporada flexible')
+   , ('Cabaña')
 ;
--- generadores/producto.py
+++ generadores/producto.py
@@ -84,6 +84,6 @@ VALUES
         
         fechas = list(random_date(startDate, 2))
 
-        file.write(f'''    , ('{x['id']}', {categories[i]} , {parque_id[i]}, '{x['name'].replace('?', '')}', '{descs[i]}', {x['min_retail_amount']}, '{fechas[0].strftime("%y/%m/%d %H:%M:%S")}', '{fechas[1].strftime("%y/%m/%d %H:%M:%S")}', {random.randint(0,1)}, {random.randint(0, 1500)}, {x['min_quantity']})\n''')
+        file.write(f'''   , ('{x['id']}', {categories[i]} , {parque_id[i]}, '{x['name'].replace('?', '')}', '{descs[i]}', {x['min_retail_amount']}, '{fechas[0].strftime("%y/%m/%d %H:%M:%S")}', '{fechas[1].strftime("%y/%m/%d %H:%M:%S")}', {random.randint(0,1)}, {random.randint(0, 1500)}, {x['min_quantity']})\n''')
         i += 1
     file.write(';')
