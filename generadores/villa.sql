0d8f12505570e88f2a6e0d13c26f8d7bac486ab9
josephaven <153795069+josephaven@users.noreply.github.com>
2023-12-21 20:29:21 -0600

    Create villa.sql

---
+++ generadores/villa.sql
@@ -0,0 +1,18 @@
+INSERT INTO villa(
+     villa_id
+     , parque_id
+     , nombre
+)
+VALUES 
+     ('1', '1', 'Villa Hollywood')
+     , ('2', '1', 'Pueblo Mexicano')
+     , ('3', '1', 'DC Super Friends')
+     , ('4', '1', 'Pueblo Francés')
+     , ('5', '1', 'Bugs Bunny Boomtown')
+     , ('6', '1', 'Pueblo Vaquero')
+     , ('7', '1', 'Pueblo Suizo')
+     , ('8', '1', 'Circo de Bugs Bunny')
+     , ('9', '1', 'Pueblo Polinesio')
+     , ('10', '1', 'Pueblo infantil')
+     , ('11', '2', 'Hurricane Harbor')
+;
