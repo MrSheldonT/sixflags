62d35ba27879fea93b989df89bb1793549e69542
RickLans <154539049+RickLans@users.noreply.github.com>
2023-12-23 09:23:27 -0600

    Add files via upload

---
+++ inserts/villas.sql
@@ -0,0 +1,10 @@
+INSERT INTO villa VALUES  
+	(1,27,'Villa Hollywood'),
+	(2,27,'Pueblo Mexicano'),
+	(3,27,'DC Super Friends'),
+	(4,27,'Pueblo Francés'),
+	(5,27,'Bugs Bunny Boomtown'),
+	(6,27,'Pueblo Vaquero'),
+	(7,27,'Pueblo Suizo'),
+	(8,27,'Circo de Bugs Bunny'),
+	(9,27,'Pueblo Polinesio');
