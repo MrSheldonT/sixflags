4223f5d67d332cd6eb9c1918e43919317014dc42
RickLans <154539049+RickLans@users.noreply.github.com>
2023-12-23 09:18:57 -0600

    Update tipo_atraccion.sql

--- inserts/tipo_atraccion.sql
+++ inserts/tipo_atraccion.sql
@@ -5,7 +5,7 @@ INSERT INTO tipo_atraccion(
     , descripcion
 )
 VALUES 
-    (1,'Juegos Familiares', 'En Six Flags Mexico la diversión familiar es nuestra especialidad. Por eso, prepárate para desahogarte y reír con ganas. Desde las alturas más extremas, hasta las aventuras más tranquilas, tenemos atracciones para que todos puedan disfrutar.')
-    , (2, 'Juegos X-Tremos', '¿Estás buscando emoción? La encontraste. De hecho, has encontrado algunas de las atracciones más rápidas, más altas y más impactantes del país incluyendo unas cuantas que baten récords mundiales.')
+     (1, 'Juegos X-Tremos', '¿Estás buscando emoción? La encontraste. De hecho, has encontrado algunas de las atracciones más rápidas, más altas y más impactantes del país incluyendo unas cuantas que baten récords mundiales.')
+    , (2,'Juegos Familiares', 'En Six Flags Mexico la diversión familiar es nuestra especialidad. Por eso, prepárate para desahogarte y reír con ganas. Desde las alturas más extremas, hasta las aventuras más tranquilas, tenemos atracciones para que todos puedan disfrutar.')
     , (3, 'Juegos Infantiles', '¡Las nuevas zonas infantiles BUGS BUNNY BOOMTOWN y DC SUPER FRIENDS ya están abiertas! Los más pequeños disfrutarán de 15 juegos y atracciones, ¡6 de ellas completamente nuevas!')
 ;
