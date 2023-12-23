997d8fa33b63264ad9ffe0764fe44d9ee4ee828c
RickLans <154539049+RickLans@users.noreply.github.com>
2023-12-23 09:28:14 -0600

    Update villas.sql

--- inserts/villas.sql
+++ inserts/villas.sql
@@ -1,10 +1,10 @@
 INSERT INTO villa VALUES  
-	(1,27,'Villa Hollywood'),
-	(2,27,'Pueblo Mexicano'),
-	(3,27,'DC Super Friends'),
-	(4,27,'Pueblo Francés'),
-	(5,27,'Bugs Bunny Boomtown'),
-	(6,27,'Pueblo Vaquero'),
-	(7,27,'Pueblo Suizo'),
-	(8,27,'Circo de Bugs Bunny'),
-	(9,27,'Pueblo Polinesio');
+	(1,1,'Villa Hollywood'),
+	(2,1,'Pueblo Mexicano'),
+	(3,1,'DC Super Friends'),
+	(4,1,'Pueblo Francés'),
+	(5,1,'Bugs Bunny Boomtown'),
+	(6,1,'Pueblo Vaquero'),
+	(7,1,'Pueblo Suizo'),
+	(8,1,'Circo de Bugs Bunny'),
+	(9,1,'Pueblo Polinesio');
