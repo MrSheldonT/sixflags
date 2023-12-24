bd082befde7d7581f403554e6bca29b0d002493c
Mundus <julianvent32@gmail.com>
2023-12-23 19:31:14 -0600

    insert de villa

---
+++ inserts/villa.sql
@@ -0,0 +1,16 @@
+INSERT INTO villa (
+	parque_id
+	, nombre
+)
+VALUES  
+	(1,'Villa Hollywood')
+	, (1,'Pueblo Mexicano')
+	, (1,'DC Super Friends')
+	, (1,'Pueblo Francés')
+	, (1,'Bugs Bunny Boomtown')
+	, (1,'Pueblo Vaquero')
+	, (1,'Pueblo Suizo')
+	, (1,'Circo de Bugs Bunny')
+	, (1,'Pueblo Polinesio')
+	, (2, 'Hurricane Harbor')
+;
