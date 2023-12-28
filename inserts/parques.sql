c8b3681669fa9455e99392e151c40b21a3c13d39
diana <zS22017039@estudiantes.uv.mx>
2023-12-27 23:53:12 -0600

    Agregué los datos de las alianzas y las excursiones

---
+++ inserts/parques.sql
@@ -0,0 +1,17 @@
+INSERT INTO parque(
+    parque_id,
+    nombre,
+    direccion,
+    telefono,
+    ciudad,
+    estado,
+    pais,
+    fecha_inauguracion,
+    fecha_cierre,
+    area_ha,
+    pagina_url,
+    mapa_url
+)
+VALUES
+    (1, 'Six Flags México', 'Carr. Picacho-Ajusco Km 1.5, Jardines del Ajusco, Tlalpan, 14219 Ciudad de México, CDMX', 'T55 5339 3600', 'CDMX', 'Abierto', 'México', '1982-03-03', NULL, 45, 'https://www.sixflags.com.mx/mexico/plan-your-visit/park-map', 'https://www.sixflags.com.mx/mexico/plan-your-visit/park-map'),
+    (2, 'Hurricane Harbor Oaxtepec', 'Autopista México-Cuautla Km. 27 + 200, Centro, 62738 Oaxtepec, Mor.','735 356 9900', 'Oaxtepec, Yautepec', 'Abierto', 'México', '2017-07-01', NULL, 0, 'https://www.sixflags.com.mx/hurricaneharborox/', 'https://www.sixflags.com.mx/hurricaneharborox/plan-your-visit/park-map');
