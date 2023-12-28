250d8bdff28f5415a534c9796486085d050813b8
josephaven <153795069+josephaven@users.noreply.github.com>
2023-12-28 02:12:38 -0600

    Create categoria_restaurante.sql

---
+++ inserts/categoria_restaurante.sql
@@ -0,0 +1,13 @@
+INSERT INTO categoria_restaurante(
+    categoria_restaurante_id
+    , nombre
+)
+VALUES 
+    ('1', 'Antojos y Bebidas')
+    , ('2', 'Café y Demás')
+    , ('3', 'Comida Americana')
+    , ('4', 'Comida Baja en Grasa')
+    , ('5', 'Comida Italiana')
+    , ('6', 'Comida Mexicana')
+    , ('7', 'Helados y postres')
+;
