c23556f5074c2a1d4c6bbd93bdc30e407627157b
josephaven <153795069+josephaven@users.noreply.github.com>
2023-12-28 22:40:42 -0600

    Update categoria_restaurante.sql

--- inserts/categoria_restaurante.sql
+++ inserts/categoria_restaurante.sql
@@ -3,11 +3,11 @@ INSERT INTO categoria_restaurante(
     , nombre
 )
 VALUES 
-    ('1', 'Antojos y Bebidas')
-    , ('2', 'Café y Demás')
-    , ('3', 'Comida Americana')
-    , ('4', 'Comida Baja en Grasa')
-    , ('5', 'Comida Italiana')
-    , ('6', 'Comida Mexicana')
-    , ('7', 'Helados y postres')
+    ('Antojos y Bebidas')
+    , ('Café y Demás')
+    , ('Comida Americana')
+    , ('Comida Baja en Grasa')
+    , ('Comida Italiana')
+    , ('Comida Mexicana')
+    , ('Helados y postres')
 ;
