0b1c0ed323e9a2db7c04dc47a5eae8592d2b7eef
josephaven <153795069+josephaven@users.noreply.github.com>
2023-12-28 22:56:49 -0600

    Update tienda.sql

--- inserts/tienda.sql
+++ inserts/tienda.sql
@@ -1,23 +1,24 @@
 INSERT INTO tienda(
     tienda_id
-    , nombre
-    , descripcion
     , villa_id
     , categoria_tienda_id
+    , nombre
+    , descripcion
+    
 )
 VALUES 
-       ('1', 'Bazar de las Banderas', 'Ubicada en la entrada principal del parque, en esta tienda encontrarás la mejor selección de artículos de Six Flags y tus Superhéroes favoritos de DC para todas las edades.','2','3')
-       , ('2', 'Capas y Personajes', 'Haz tu propio slime, explota tu creatividad armando figuras de bloques y conviértete en un superhéroe o superheroína con la variedad de disfraces y souvenirs disponibles.','5','3')
-       , ('3', 'Coaster Candy', 'Ubicada a la entrada de la Villa Hollywood, en esta tienda encontrarás una gran variedad de dulces, gomitas, paletas, algodón de azúcar, manzanas cubiertas y más.','1','3')
-       , ('4', 'DC Superhero Store', 'Ubicada a un costado de Supergirl Sky Flight, esta tienda será tu mejor opción para encontrar prendas y artículos de tus personajes favoritos de DC Comics.','1','2')
-       , ('5', 'El Castillo de los Sueños', 'Disfruta con tus hijos de una sesión súper divertida de fantasía con fotos, maquillaje, peinado, manicure y otras sorpresas, creando momentos de complicidad entrañables.','5','2')
-       , ('6', 'Medusa Steel Coaster Shop', 'Una vez que desafiaste a nuestra increíble montaña rusa híbrida, no olvides llevarte un recuerdo de tu hazaña en esta tienda.','6','4')
-       , ('7', 'Mercado de los Looney Tunes', '¿Eres fan de los Looney Tunes?, aquí podrás encontrar la más grande variedad de artículos de tus personajes favoritos.','2','2')
-       , ('8', 'Pacific Coast Surf Shop', 'Ubicada frente a CraZanity, esta tienda es tu mejor opción para adquirir sudaderas, capas, gorras, sombrillas, impermeables y mucho más.','1','3')
-       , ('9', 'Pirulí', 'Desde dulces, gomitas a granel, botanas y chocolates, hasta dulces típicos y souvenirs de tus personajes favoritos.','2','1')
-       , ('10', 'Superhéroes y Villanos', 'Después de la gran experiencia de The Dark Knight Coaster, no puedes perder la oportunidad de llevarte un recuerdo de tu villano o superhéroe favorito.','6','3')
-       , ('11', 'Superman: La Tienda de Metrópolis', 'En esta tienda encontrarás miles de artículos de Superman y más personajes del Universo DC, desde un bolígrafo hasta chamarras de colección.','2','3')
-       , ('12', 'The Fun House Shop', 'Un rincón especialmente dedicado al enemigo No. 1 de Batman, The Joker y su acompañante inseparable Harley Quinn.','4','3')
-       , ('13', 'Big Surf Shop', '¡Recuerda tu visita! Si pasaste un día lleno de inolvidables aventuras, estamos seguros que en Big Surf Shop encontrarás algún detalle para recordar tu visita o para esa persona especial.','11','4')
-       , ('14', 'Cyclone Supply', 'Renta tu llanta, compra lo que necesites para complementar tu visita o lleva un souvenir para regalar a esa persona especial.','11','4')
-       , ('15', 'Island Traders', 'Si se te olvidó algo que necesitas durante el día o simplemente quieres un increíble souvenir para hacer de tu visita algo inolvidable, ¡no te preocupes!','11','4');
+       ('2','3','Bazar de las Banderas', 'Ubicada en la entrada principal del parque, en esta tienda encontrarás la mejor selección de artículos de Six Flags y tus Superhéroes favoritos de DC para todas las edades.')
+       , ('5','3','Capas y Personajes', 'Haz tu propio slime, explota tu creatividad armando figuras de bloques y conviértete en un superhéroe o superheroína con la variedad de disfraces y souvenirs disponibles.')
+       , ('1','3','Coaster Candy', 'Ubicada a la entrada de la Villa Hollywood, en esta tienda encontrarás una gran variedad de dulces, gomitas, paletas, algodón de azúcar, manzanas cubiertas y más.')
+       , ('1','2','DC Superhero Store', 'Ubicada a un costado de Supergirl Sky Flight, esta tienda será tu mejor opción para encontrar prendas y artículos de tus personajes favoritos de DC Comics.')
+       , ('5','2','El Castillo de los Sueños', 'Disfruta con tus hijos de una sesión súper divertida de fantasía con fotos, maquillaje, peinado, manicure y otras sorpresas, creando momentos de complicidad entrañables.')
+       , ('6','4''Medusa Steel Coaster Shop', 'Una vez que desafiaste a nuestra increíble montaña rusa híbrida, no olvides llevarte un recuerdo de tu hazaña en esta tienda.')
+       , ('2','2','Mercado de los Looney Tunes', '¿Eres fan de los Looney Tunes?, aquí podrás encontrar la más grande variedad de artículos de tus personajes favoritos.')
+       , ('1','3''Pacific Coast Surf Shop', 'Ubicada frente a CraZanity, esta tienda es tu mejor opción para adquirir sudaderas, capas, gorras, sombrillas, impermeables y mucho más.')
+       , ('2','1','Pirulí', 'Desde dulces, gomitas a granel, botanas y chocolates, hasta dulces típicos y souvenirs de tus personajes favoritos.')
+       , ('6','3''Superhéroes y Villanos', 'Después de la gran experiencia de The Dark Knight Coaster, no puedes perder la oportunidad de llevarte un recuerdo de tu villano o superhéroe favorito.')
+       , ('2','3','Superman: La Tienda de Metrópolis', 'En esta tienda encontrarás miles de artículos de Superman y más personajes del Universo DC, desde un bolígrafo hasta chamarras de colección.')
+       , ('4','3','The Fun House Shop', 'Un rincón especialmente dedicado al enemigo No. 1 de Batman, The Joker y su acompañante inseparable Harley Quinn.')
+       , ('11','4','Big Surf Shop', '¡Recuerda tu visita! Si pasaste un día lleno de inolvidables aventuras, estamos seguros que en Big Surf Shop encontrarás algún detalle para recordar tu visita o para esa persona especial.')
+       , ('11','4','Cyclone Supply', 'Renta tu llanta, compra lo que necesites para complementar tu visita o lleva un souvenir para regalar a esa persona especial.')
+       , ('11','4','Island Traders', 'Si se te olvidó algo que necesitas durante el día o simplemente quieres un increíble souvenir para hacer de tu visita algo inolvidable, ¡no te preocupes!');
